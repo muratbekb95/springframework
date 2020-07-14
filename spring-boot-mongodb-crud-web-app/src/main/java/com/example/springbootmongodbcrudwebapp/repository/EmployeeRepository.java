25879f06f03e48d9b6ae78f21f24832c18b58e10
muratbekb95 <muratbekb95@gmail.com>
2020-07-15 02:32:23 +0600

    Remove odd files

--- spring-boot-mongodb-crud-web-app/src/main/java/com/example/springbootmongodbcrudwebapp/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.springbootmongodbcrudwebapp.repository;
-
-import com.example.springbootmongodbcrudwebapp.model.Employee;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmployeeRepository extends MongoRepository<Employee, Long> {
-}
